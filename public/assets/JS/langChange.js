3b0a6b9530041df557ccb59103910630bd6f21fb
tiodio324 <nikita324smirnovvv@gmail.com>
2025-04-23 23:39:08 +0300

    improve language change

--- public/assets/JS/langChange.js
+++ public/assets/JS/langChange.js
@@ -3,34 +3,39 @@ const allLang = [
     "ru",
     "en"
 ];
+const DEFAULT_LANG = "ru";
 
-select.addEventListener('change', changeURLLanguage);
-
-function changeURLLanguage() {
-    let lang = select.value;
-
-    location.href = window.location.pathname + '#' + lang;
-    location.reload();
-}
+select.addEventListener('change', changeLanguage);
 
 function changeLanguage() {
-    let hash = window.location.hash;
-
-    hash = hash.substring(1);
-    if (!allLang.includes(hash)) {
-        location.href = window.location.pathname + '#ru';
-        location.reload();
+    let lang = select.value;
+    
+    if (allLang.includes(lang)) {
+        localStorage.setItem('userLanguage', lang);
+        applyLanguage(lang);
     }
-    select.value = hash;
+}
+
+function applyLanguage(lang) {
+    select.value = lang;
 
     for (let key in langArr) {
         let elem = document.querySelector('.lng-' + key);
         if (elem) {
-            elem.innerHTML = langArr[key][hash];
+            elem.innerHTML = langArr[key][lang];
         } else {
             console.warn(`Element with selector 'lng-${key}' not found.`);
         }
     }
 }
 
-changeLanguage();
+document.addEventListener('DOMContentLoaded', () => {
+    let savedLang = localStorage.getItem('userLanguage');
+
+    if (!savedLang || !allLang.includes(savedLang)) {
+        savedLang = DEFAULT_LANG;
+        localStorage.setItem('userLanguage', savedLang);
+    }
+
+    applyLanguage(savedLang);
+});
